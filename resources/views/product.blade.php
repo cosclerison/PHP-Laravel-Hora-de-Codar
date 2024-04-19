fe0e048e75105bbfd0aebd22aabe7bd449c07f25
Clerison <cosclerison@gmail.com>
2024-04-18 23:15:46 -0300

    :hammer:Criando layout com blade

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,4 +1,12 @@
+@extends('layouts.main')
+
+@section('title', "Produtos")
+
+@section("content")
+
 <h1>Pagina de produtos...</h1>
 <a href="/"><strong>HOME</strong></a>
 <br>
 <a href="/contact"><strong>Contatos</strong></a>
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,49 +1,42 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
-        <!-- Styles CSS -->
-        <link rel="stylesheet" href="/css/styles.css">
-        <!-- Script JS -->
-        <script src="/js/script.js"></script>
-    </head>
-    <body class="antialiased">
-        <h1>HOME</h1>
-        <img src="/img/banner.jpg" alt="banner" title="Banner">
-        <p>Author: {{$nome}}</p>
-        @if(10 > 5)
-            <h3>Resultado TRUE</h3>
-        @endif
+@section('title', "HDC Events")
 
-        @if($nome == "clerison")
-            <h3>O nome é {{$nome}}, tem {{$idade}} anos.</h3>
-            <h3>Sua profissão é {{$profissao}}.</h3>
-        @elseif($nome == "juliana")
-            <h3>O nome é {{$nome}}</h3>
-        @else
-            <h3>O nome não é identificado</h3>
-        @endif
+@section("content")
 
-        @for($i = 0; $i < count($arr); $i++)
-            <p> Valor = {{ $arr[$i]}}, indice = {{$i}}</p>
-            @if($i == 2)
-                <p>O indice é 2</p>
-            @endif
-        @endfor
+<h1>HOME</h1>
+<img src="/img/banner.jpg" alt="banner" title="Banner">
+<p>Author: {{$nome}}</p>
+@if(10 > 5)
+    <h3>Resultado TRUE</h3>
+@endif
 
-        @php
-            $name = "João";
-            echo $name;
-        @endphp
+@if($nome == "clerison")
+    <h3>O nome é {{$nome}}, tem {{$idade}} anos.</h3>
+    <h3>Sua profissão é {{$profissao}}.</h3>
+@elseif($nome == "juliana")
+    <h3>O nome é {{$nome}}</h3>
+@else
+    <h3>O nome não é identificado</h3>
+@endif
 
-        @foreach($nomes as $nome)
-            <p>{{$loop->index}} {{$nome}}</p>
-        @endforeach
+@for($i = 0; $i < count($arr); $i++)
+    <p> Valor = {{ $arr[$i]}}, indice = {{$i}}</p>
+    @if($i == 2)
+        <p>O indice é 2</p>
+    @endif
+@endfor
 
-        <!-- Comentário do HTML -->
-        {{-- Comentário do BLADE --}}
-    </body>
-</html>
+@php
+    $name = "João";
+    echo $name;
+@endphp
+
+@foreach($nomes as $nome)
+    <p>{{$loop->index}} {{$nome}}</p>
+@endforeach
+
+<!-- Comentário do HTML -->
+{{-- Comentário do BLADE --}}
+
+@endsection
