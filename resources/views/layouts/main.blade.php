223d12a633bfa46dc0bed41ee00210a565a185dd
Clerison <cosclerison@gmail.com>
2024-04-19 23:46:04 -0300

    :hammer:Resgatando parâmetros de URL

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,8 +18,32 @@
         <script src="/js/script.js"></script>
 
     </head>
-    <body class="antialiased">
-       @yield('content')
-    <footer>HDC Events &copy; 2024</footer>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="/img/hdcevents_logo.svg" alt="Logo">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Criar Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Cadastrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+        @yield('content')
+        <footer>HDC Events &copy; 2024</footer>
     </body>
 </html>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.main')
 
-@section('title', "Produto")
+@section('title', "Produtos")
 
 @section("content")
-<!-- Inicio -->
 
-    @if($id != null)
-        <p>Exibindo Produto id: {{ $id }}</p>
+<h1>Pagina de produtos...</h1>
+    @if($busca != '')
+        <p>O usuário está buscando por: {{ $busca }}</p>
     @endif
-
 <a href="/"><strong>HOME</strong></a>
 <br>
 <a href="/contact"><strong>Contatos</strong></a>
