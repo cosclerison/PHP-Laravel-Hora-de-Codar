4b52d3541c24742028773103afabd2f9ad0d3d47
Clerison <cosclerison@gmail.com>
2024-05-02 18:05:25 -0300

    :hamme:Salvando datas no banco de dados

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,13 +23,16 @@
         <div class="card col-md-3">
             <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
             <div class="card-body">
-                <p class="card-date">01/05/2024</p>
+                <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-participants">X Participantes</p>
                 <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
             </div>
         </div>
         @endforeach
+        @if(count($events) == 0)
+            <p class="btn-danger">Não há eventos disponiveis</p>
+        @endif
     </div>
 </div>
 @endsection
